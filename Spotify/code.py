0422b33d577c42f904718bddbb8f3dab0964dd8e
zidelisd <zidelisd@wit.edu>
2018-06-17 15:42:46 -0400

    code to create a playlist

--- Spotify/code.py
+++ Spotify/code.py
@@ -3,11 +3,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import pprint
 
-
-if len(sys.argv) > 1:
-    search_str = sys.argv[1]
-else:
-    search_str = 'Fall Out Boy'
+search_str = input("Enter name of artist you would like to search?  ")
+#if len(sys.argv) > 1:
+ #   search_str = sys.argv[1]
+#else:
+ #   search_str = 'Fall Out Boy'
 
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
