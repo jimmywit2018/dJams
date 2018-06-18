ecbd90e511d15b426639b2d1761f1ea0f7a16938
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 01:42:22 -0400

    different way to use public

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -13,11 +13,11 @@ playlist_description = input("Short description of the playlist? : ")
 username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
-
+public = true
 
 token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b")
 
 sp = spotipy.Spotify(auth=token)
 sp.trace = False
-playlists = sp.user_playlist_create(username, playlist_name, playlist_description, public=False)
+playlists = sp.user_playlist_create(username, playlist_name, public, playlist_description)
 #pprint.pprint(playlists)
