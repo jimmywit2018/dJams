dc6cf04eb9dcbd7fa6530122e99a87e7cdb37ed7
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 00:23:08 -0400

    fix

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -9,7 +9,7 @@ with open ('clientID.txt', 'rt') as in_file:
     client_id = in_file.read()
 with open ('clientSecret.txt', 'rt') as in_file:
     client_secret = in_file.read()
-token = util.prompt_for_user_token(username, scope, client_id , client_secret , redirect_uri="http://localhost:8888/callback/")
+token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret , redirect_uri="http://localhost:8888/callback/")
 print(client_id)
 print(contents_client_id)
 if token:
