6846d72851e4f5b1264f57a77214e80ec392d656
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 01:42:40 -0400

    fixed capitalization

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -13,7 +13,7 @@ playlist_description = input("Short description of the playlist? : ")
 username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
-public = true
+public = True
 
 token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b")
 
