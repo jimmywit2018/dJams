e49eb5e63a76588e31f044c3dabe6c74d76fe4dc
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 00:27:47 -0400

    got rid of unneeded print statements

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -10,8 +10,7 @@ with open ('clientID.txt', 'rt') as in_file:
 with open ('clientSecret.txt', 'rt') as in_file:
     client_secret = in_file.read()
 token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
-print(client_id)
-print(contents_client_id)
+
 if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks()
