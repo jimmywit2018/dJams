f1c26afd64e595ac5936f87071a6b39b2c26a975
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 00:33:03 -0400

    currectly set the userID

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -9,7 +9,7 @@ spotify = spotipy.Spotify()
 #to fix
 playlistName = input("What is the name of the playlist you wish to create")
 playlistDescription = input("Short description of the playlist")
-userID = h0m596l5gz014wayiyy29p0gg
+userID = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 
