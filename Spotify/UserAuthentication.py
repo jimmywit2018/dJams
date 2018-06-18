9d124c06243faa0f92537cb96973df34b58399ac
Daniel Zidelis <zidelisd@wit.edu>
2018-06-17 23:42:50 -0400

    added a print to test something

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -11,8 +11,8 @@ print(contents_client_id)
 with open ('clientSecret.txt', 'rt') as in_file:
     contents_client_secret = in_file.read()
 print(contents_client_secret)
-token = util.prompt_for_user_token(username, scope, client_id = c"b7642ea152d44cbf95e9d7efd223cc49", client_secret = contents_client_secret, redirect_uri="http://google.com/")
-
+token = util.prompt_for_user_token(username, scope, client_id = "b7642ea152d44cbf95e9d7efd223cc49", client_secret = contents_client_secret, redirect_uri="http://google.com/")
+print(client_id)
 if token:
     sp = spotipy.Spotify(auth=token)
     results = sp.current_user_saved_tracks()
