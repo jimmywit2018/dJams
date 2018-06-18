e59baa16a29874f3c9c47442799a77dc890beb30
zidelisd <zidelisd@wit.edu>
2018-06-17 22:24:48 -0400

    UserAuthentication.py fix

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -11,7 +11,7 @@ username= input("Enter user name")
 #    sys.exit()
 
 #something is up with the userauthentication
-token = util.prompt_for_user_token(client_id=username, scope=scope, redirect_uri="http//google.com/")
+token = util.prompt_for_user_token(username, scope= scope, redirect_uri="http//google.com/")
 
 if token:
     sp = spotipy.Spotify(auth=token)
