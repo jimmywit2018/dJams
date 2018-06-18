77b0a67ef44f83da9d3dd783704d445976454cc5
zidelisd <zidelisd@wit.edu>
2018-06-17 21:55:50 -0400

    UserAuthentication.py fix
    Fixed error in code of UserAuthentication class for testing purposes.

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -7,7 +7,7 @@ scope = 'user-library-read'
 if len(sys.argv) > 1:
     username = sys.argv[1]
 else:
-    print "Usage: %s username" % (sys.argv[0],)
+    print("Usage: %s username" % (sys.argv[0],))
     sys.exit()
 
 token = util.prompt_for_user_token(username, scope)
@@ -19,4 +19,4 @@ if token:
         track = item['track']
         print track['name'] + ' - ' + track['artists'][0]['name']
 else:
-    print "Can't get token for", username
+    print("Can't get token for", username)
