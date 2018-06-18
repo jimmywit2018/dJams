d83f822200567a1b4975e51ef32e8f560520a104
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 01:57:20 -0400

    got rid of obsolete code, try to print

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -9,15 +9,13 @@ import spotipy.util as util
 scope = 'playlist-modify-private'
 spotify = spotipy.Spotify()
 playlist_name = input("What is the name of the playlist you wish to create? : ")
-playlist_description = input("Short description of the playlist? : ")
 username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
-public = True
 
 token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b")
 
 sp = spotipy.Spotify(auth=token)
 sp.trace = False
 sp.user_playlist_create(username, playlist_name, public=False)
-#pprint.pprint(playlists)
+pprint.pprint(playlists)
