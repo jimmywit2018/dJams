2e689acd5110ccd106be088a220f128c47b2ece6
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 02:54:02 -0400

    edit

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -16,16 +16,16 @@ username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 
-token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b")
+token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b", cache_path = ".cache-h0m596l5gz014wayiyy29p0gg")
 
 sp = spotipy.Spotify(auth=token)
 sp.trace = False
-sp.user_playlist_create(username, playlist_name, public=False)
+sp.user_playlist_create(username, playlist_name, public=False,)
 
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-track = input("Name of track you would like to add to playlist: ")
-artist = input("Who is the artist of the track? ")
-track_id= sp.search(q='artist' + artist + 'track:' + track, type='track')
-#pprint.pprint(track_id)
-print(track_id)
+# client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+# sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+# track = input("Name of track you would like to add to playlist: ")
+# artist = input("Who is the artist of the track? ")
+# track_id= sp.search(q='artist' + artist + 'track:' + track, type='track')
+# #pprint.pprint(track_id)
+# print(track_id)
