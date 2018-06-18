3b1cdb36dd6abcb4bb0f9c51532609bc874602f0
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 00:51:31 -0400

    changed name of code.py to search.py, editted CreatePlaylist.py

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -4,9 +4,8 @@ import spotipy
 import pprint
 #fix code so the class can be used to be called by a main class
 
+scope = 'user-library-read'
 spotify = spotipy.Spotify()
-#something is going on with the user id where it is not accepted need
-#to fix
 playlistName = input("What is the name of the playlist you wish to create? : ")
 playlistDescription = input("Short description of the playlist? : ")
 userID = "h0m596l5gz014wayiyy29p0gg"
@@ -14,6 +13,14 @@ client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 
 #once get create to work work on adding to it
-spotipy.oauth2.SpotifyClientCredentials.get_access_token(self)
-sp = spotipy.Spotify(token)
+token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
+        track = item['track']
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
 sp.user_playlist_create(userID, playlistName, public=True)
