890959de9bc6aa9015353945cdf235430364c557
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 01:05:01 -0400

    CreatePlaylist.py change

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -14,7 +14,7 @@ client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 
 #once get create to work work on adding to it
-token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
+token = util.get_access_token()
 
 if token:
     sp = spotipy.Spotify(auth=token)
