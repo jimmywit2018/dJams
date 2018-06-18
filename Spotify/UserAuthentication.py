4afe2c985c8f14f95ffe5a14854718ea3af29a9b
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 02:32:41 -0400

    commented out coee

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -5,10 +5,10 @@ import spotipy.util as util
 scope = 'user-library-read'
 username= input("Enter user name: ")
 
-with open ('clientID.txt', 'rt') as in_file:
-    client_id = in_file.read()
-with open ('clientSecret.txt', 'rt') as in_file:
-    client_secret = in_file.read()
+#with open ('clientID.txt', 'rt') as in_file:
+#    client_id = in_file.read()
+#with open ('clientSecret.txt', 'rt') as in_file:
+#    client_secret = in_file.read()
 token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
 
 if token:
