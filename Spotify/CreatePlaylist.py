119fae5b87cf27b3c547d5c4a19c67391b1edbb1
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 02:54:52 -0400

    de

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -16,7 +16,7 @@ username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 
-token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b", cache_path = ".cache-h0m596l5gz014wayiyy29p0gg")
+token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b")
 
 sp = spotipy.Spotify(auth=token)
 sp.trace = False
