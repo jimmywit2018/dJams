328070e8961510f7c63692dfc7a881529c705e80
Daniel Zidelis <zidelisd@wit.edu>
2018-06-18 01:20:55 -0400

    Changed names of playlist name/description in CreatePlaylist.py

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -9,8 +9,8 @@ import spotipy.util as util
 
 scope = 'playlist-modify-private'
 spotify = spotipy.Spotify()
-playlistName = input("What is the name of the playlist you wish to create? : ")
-playlistDescription = input("Short description of the playlist? : ")
+playlist_name = input("What is the name of the playlist you wish to create? : ")
+playlist_description = input("Short description of the playlist? : ")
 username = "h0m596l5gz014wayiyy29p0gg"
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
 client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
@@ -21,7 +21,7 @@ token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf9
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
-    playlists = sp.user_playlist_create(username, playlist_name,
+    playlists = sp.user_playlist_create(username, playlis_name,
                                         playlist_description)
     pprint.pprint(playlists)
 else:
