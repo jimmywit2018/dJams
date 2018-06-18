49edf39f320bd76e8029747e3bdbeb5eea194c46
zidelisd <zidelisd@wit.edu>
2018-06-17 22:00:44 -0400

    UserAuthentication.py change
    Changed UserAuthentication class to take in input from user to test with my username... side note, Spotify has... CRAPPY.... user names.

--- Spotify/UserAuthentication.py
+++ Spotify/UserAuthentication.py
@@ -3,12 +3,12 @@ import spotipy
 import spotipy.util as util
 
 scope = 'user-library-read'
-
-if len(sys.argv) > 1:
-    username = sys.argv[1]
-else:
-    print("Usage: %s username" % (sys.argv[0],))
-    sys.exit()
+username= input("Enter user name")
+#if len(sys.argv) > 1:
+#    username = sys.argv[1]
+#else:
+#    print("Usage: %s username" % (sys.argv[0],))
+#    sys.exit()
 
 token = util.prompt_for_user_token(username, scope)
 
