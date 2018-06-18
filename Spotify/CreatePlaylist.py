f6bf12ccd93ac426a34f340046e6e8dea5c6b549
zidelisd <zidelisd@wit.edu>
2018-06-18 10:27:48 -0400

    changed collab to false

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -23,7 +23,7 @@ sp.trace = False
 sp.user_playlist_create(username, playlist_name, public=False,)
 
 playlist_id = input("What is the id of the playlist you wish to add songs to? : ")
-results = sp.user_playlist_change_details(username, playlist_id, name=playlist_name, public=False,collaborative=collaborative, description=description)
+results = sp.user_playlist_change_details(username, playlist_id, name=playlist_name, public=False,collaborative=False)
 print(results)
 
 # track_ids="spotify:track:2lUA2flB94XburZIe7BmHZ"
