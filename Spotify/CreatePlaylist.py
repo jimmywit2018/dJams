59a4ca4e5c810fd9ee42908f4325b8d78c381618
Daniel Zidelis <zidelisd@wit.edu>
2018-07-02 11:33:38 -0400

    sp.trace in code

--- Spotify/CreatePlaylist.py
+++ Spotify/CreatePlaylist.py
@@ -19,7 +19,7 @@ client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
 token = util.prompt_for_user_token(username, scope="playlist-modify-private", client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b", redirect_uri="http://google.com/")
 
 sp = spotipy.Spotify(auth=token)
-sp.trace = False
+sp.trace = False 
 sp.user_playlist_create(username, playlist_name, public=False,)
 
 # results = sp.user_playlist_change_details(username, playlist_id, name=playlist_name, public=public,collaborative=collaborative, description=description)
