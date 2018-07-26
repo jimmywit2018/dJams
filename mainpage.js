9090e9f9d77248e9e5f640736685966542de2ff8
James Parsons <parsonsj3@wit.edu>
2018-07-26 13:06:21 -0400

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,15 @@
+window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
+    window.myWidgetParam.push({
+        id: 5,
+        cityid: '4930956',
+        appid: '64e1289a90c76ae161b66981615e5d2d',
+        units: 'imperial',
+        containerid: 'openweathermap-widget-5',
+    });
+    (function() {
+        var script = document.createElement('script');
+        script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        var s = document.getElementsByTagName('script')[0];
+    })();
