52a7395a912ec91c8318b2beabadd913b58d1935
James Parsons <parsonsj3@wit.edu>
2018-07-30 13:41:13 -0400

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,15 +1,17 @@
 window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-    window.myWidgetParam.push({
-        id: 5,
-        cityid: '4930956',
-        appid: '64e1289a90c76ae161b66981615e5d2d',
-        units: 'imperial',
-        containerid: 'openweathermap-widget-5',
-    });
-    (function() {
-        var script = document.createElement('script');
-        script.async = true;
-        script.charset = "utf-8";
-        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-        var s = document.getElementsByTagName('script')[0];
-    })();
+window.myWidgetParam.push({
+    id: 5,
+    cityid: '4930956',
+    appid: '64e1289a90c76ae161b66981615e5d2d',
+    units: 'imperial',
+    containerid: 'openweathermap-widget-5',
+});
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.charset = "utf-8";
+    script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+    s.style.textAlign = 'center';
+})();
