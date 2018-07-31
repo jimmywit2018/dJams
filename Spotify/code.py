72f05c73fa442ba994f629c54e65e2bec92e8226
zidelisd <zidelisd@wit.edu>
2018-07-31 15:05:36 -0400

    Moved base help code to different folder
    Moved code.py to Spotify folder, deleted Spotify Python folder

---
+++ Spotify/code.py
@@ -0,0 +1,19 @@
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import pprint
+
+search_str = input("Name of artist you wish to search for")
+#if len(sys.argv) > 1:
+    #search_str = sys.argv[1]
+#else:
+    #search_str = 'Kendrick'
+
+client_id = "b7642ea152d44cbf95e9d7efd223cc49"
+client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+result = sp.search(search_str)
+pprint.pprint(result)
