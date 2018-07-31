548d3eb0f3a73971f528d408ee048638a9867341
zidelisd <zidelisd@wit.edu>
2018-07-31 15:04:18 -0400

    Notes for Rovi
    Commented notes for new plan on how to get response from Rovi music search API/ recieve JSON data/parse through to get artist name and song name

--- Spotify/Rovi.py
+++ Spotify/Rovi.py
@@ -1,9 +1,14 @@
 import requests
+#create url variables for every type of api call
 
 #make a get request to get the music of mood happy?
 response = requests.get("http://api.rovicorp.com/data/v1.1/descriptor/significantsongs?apikey=u8b9c5u6cwwt3mnns9ubdah6&sig=sig&genreids=MA0000002533,MA0000002960")
 
 print(response.status_code)
+
+#figure out how to get back JSON data
+
+#parse said data to get just the song name and artist name
 
 # Print the content of the response (the data the server returned)
 print(response.content)
