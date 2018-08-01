6461a597d3224362a1b9674850e864e4b3b55c94
zidelisd <zidelisd@wit.edu>
2018-08-01 08:57:35 -0400

    Getting Playlist URI
    Getting a working response code from Spotify API call, now just need to get response data back and pull needed information from it.

--- Spotify/code.py
+++ Spotify/code.py
@@ -1,6 +1,6 @@
 import requests
 import json
 
-response = request.get(curl -X "GET" "https://api.spotify.com/v1/users/h0m596l5gz014wayiyy29p0gg/playlists" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQBY_g1uXrEMgEhC6tXRrc61rud0eeP5bUC8BwvWY2LJxUy5E7NMJEaZ45t5W8R-cAcs8MoPHj0QIT8Shub42zDKC-kwbUXprBOMwoRVlr1bzcRAf11pQpCqVqBm_921hxk0gBKX4ANmB4OXOQKpSlQtRHpFxUQtjNxyx4Z02NMl2mxr")
-
+#use playlistid to get the playlist uri which is what is needed for the spotify playlist?
+response = requests.get("https://api.spotify.com/v1/users/h0m596l5gz014wayiyy29p0gg/playlists/59ZbFPES4DQwEjBpWHzrtC")
 print(response.status_code)
