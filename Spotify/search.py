8e62b4efa7d5ddeeb46f08c9999aa1928a745f41
Daniel Zidelis <zidelisd@wit.edu>
2018-08-04 15:03:07 -0400

    Spotify Search song
    able to change the search query

--- Spotify/search.py
+++ Spotify/search.py
@@ -7,30 +7,26 @@ import requests
 
 scope = "user-read-private"
 username = 'h0m596l5gz014wayiyy29p0gg'
-search_str = 'Radiohead'
-search_str = input("Enter name of artist you would like to search?  ")
-#if len(sys.argv) > 1:
- #   search_str = sys.argv[1]
-#else:
- #   search_str = 'Fall Out Boy'
+
+search_str_song = input("Enter the name of the song you would like to search: ")
+search_str_art = input("Enter name of artist of the song:  ")
 
 client_id = "b7642ea152d44cbf95e9d7efd223cc49"
-client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b"
-token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
-#client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-#sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+client_secret = '1094e61f08a845a6b1e9a651fe9a1e2b'
+token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = client_secret , redirect_uri="http://google.com/")
 
 #call API
 sp = spotipy.Spotify(auth=token)
-print(token)
+
 headers = {"Authorization": "Bearer " + token, "Accept": "application/json", "Content-Type": "application/json"}
-#url = 'https://api.spotify.com/v1/search?q=track:%20artist:bieber&type=track&Authorization:'
-response = requests.get("https://api.spotify.com/v1/search?q=track:despacito%20artist:bieber&type=track", headers=headers)
-#print("https://api.spotify.com/v1/search?q=track:%20artist:bieber&type=track&Authorization: Bearer " + token)
-#headers = {'Authorization: Bearer BQDGm5E3KcVPrn74WjOpGMqyx_sWkgJZBvriHVP5Wy6In1gy25lQP-qA3dJL6I3rV5CPr6jwqyMniJvamItVnbFqG1rLII-mkuD6dU6rEiOYTg8WX9cg4yhBmO_8wEHOCbDr5TL2g0Vn1irXis1YGNPZD2VWRvG8Hh9Yv2-kG_5C_gB89KoHDW_PfaM'}
-#r = requests.post(url,headers=headers)
+url = "https://api.spotify.com/v1/search?q=track:"+search_str_song+"%20artist:"+search_str_art+"&type=track"
+response = requests.get(url, headers=headers)
+
 print(response)
 print(response.text)
-#print("hello")
+
+# playresponse = requests.get("https://api.spotify.com/v1/search?q=%22durga11%22&type=playlist", headers=headers)
+# print(playresponse)
+# print(playresponse.text)
 #result = sp.search(search_str)
 #pprint.pprint(result)
