0b24bb0c2ade039b926ec23d7cd57d4385d21922
Daniel Zidelis <zidelisd@wit.edu>
2018-08-05 18:01:43 -0400

    test.py
    testing responses for music entry.

--- Spotify/test.py
+++ Spotify/test.py
@@ -1,14 +1,14 @@
-# shows artist info for a URN or URL
-import spotipy.util as util
-import spotipy
 import sys
 import pprint
+import requests
 
-scope = "user-read-private"
-username = "h0m596l5gz014wayiyy29p0gg"
-search_str = 'Radiohead'
-token = util.prompt_for_user_token(username, scope, client_id="b7642ea152d44cbf95e9d7efd223cc49", client_secret = "1094e61f08a845a6b1e9a651fe9a1e2b" , redirect_uri="http://google.com/")
+apiK= "7d02f117f44c9fcc4205c77ef98e6288"
+apiS= "2fbb022389b40a3d6efcc9faf23432aa"
 
-sp = spotipy.Spotify()
-result = sp.search(search_str)
-pprint.pprint(result)
+#call API
+responsetop = requests.get("https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=disco&limit=2&api_key="+apiK+"&format=json")
+responseweek = requests.get("https://ws.audioscrobbler.com/2.0/?method=tag.getweeklychartlist&tag=disco&api_key="+apiK+"&format=json")
+print(responsetop.status_code)
+print(responsetop.text)
+print(responseweek.status_code)
+print(responseweek.text)
