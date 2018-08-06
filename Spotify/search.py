4e6808a7dd3bade6794f4cb57944c967da1d114f
Daniel Zidelis <zidelisd@wit.edu>
2018-08-06 03:37:39 -0400

    search.py
    now searches for users playlists and playlistids

--- Spotify/search.py
+++ Spotify/search.py
@@ -8,64 +8,36 @@ import json
 import mmap
 import re
 
-scope = "user-read-private"
+scope = "playlist-modify-public"
 username = 'h0m596l5gz014wayiyy29p0gg'
 
-search_str_song = input("Enter the name of the song you would like to search: ")
-search_str_art = input("Enter name of artist of the song:  ")
 
 client_id = 'b7642ea152d44cbf95e9d7efd223cc49'
 client_secret = '1094e61f08a845a6b1e9a651fe9a1e2b'
 token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret = client_secret , redirect_uri="http://google.com/")
 
-#call API
 sp = spotipy.Spotify(auth=token)
 
 headers = {"Authorization": "Bearer " + token, "Accept": "application/json", "Content-Type": "application/json"}
-url = "https://api.spotify.com/v1/search?q=track:"+search_str_song+"%20artist:"+search_str_art+"&type=track"
+url = "https://api.spotify.com/v1/users/"+username+"/playlists"
 response = requests.get(url, headers=headers)
-
-#print(response)
+print(response.status_code)
 #print(response.text)
 
-#loads the response into a json_data
 json_data= json.loads(response.text)
-with open('search.json', 'w') as outfile:
-        json.dump(json_data, outfile,indent=4, sort_keys=True)
-with open('search.json') as infile:
-    data = json.loads(infile.read())
-
-#if "spotify:track:" in open('search.json').read():
-    #print("true")
-#
+with open('playlists.json', 'w') as outfile:
+         json.dump(json_data, outfile,indent=4, sort_keys=True)
+with open('playlists.json') as infile:
+     dataplaylist = json.loads(infile.read())
 iter = 0
-for track in data['tracks']:
+for track in dataplaylist['items']:
+
     print(len(track))
-    while iter < len(track):
-        track_uri = data['tracks']['items'][0]['uri']
+    while iter < 5:
+        print(iter)
+        playlist_uri= dataplaylist['items'][iter]['uri']
+        print(playlist_uri)
         iter= iter+1
-print(items)
-outfile.close()
-infile.close()
-# file = open('search.json', 'r+b')
-# mf = mmap.mmap(file.fileno(), 0)
-# mf.seek(0) # reset file cursor
-# search = "spotify:track:"
-# search_as_byte = str.encode(search)
-# #print(type(search_as_byte))\
-# m = re.search(search_as_byte, mf)
-# #print(m.span())
-# mf.seek(m.end())
-# search_id = "\""
-# search_id_as_byte = str.encode(search_id)
-# #print(search_id_as_byte)
-# i = re.search(search_id_as_byte, mf)
-# #print(i.start(), i.end())
-#
-# mf.close()
-# file.close()
-# playresponse = requests.get("https://api.spotify.com/v1/search?q=%22durga11%22&type=playlist", headers=headers)
-# print(playresponse)
-# print(playresponse.text)
-#result = sp.search(search_str)
-#pprint.pprint(result)
+# print(items)
+# outfile.close()
+# infile.close()
