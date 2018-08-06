1b51d35c161d417afdd8ed3ed35eb37a22926572
Daniel Zidelis <zidelisd@wit.edu>
2018-08-06 07:39:44 -0400

    djams database and surveyanswers

---
+++ PythonDatabase/surveyanswers.py
@@ -0,0 +1,47 @@
+import sqlite3
+import json
+
+path = r'C:\Users\zidel\Documents\GitHub\dJams\database\djams.db'
+conn = sqlite3.connect(path)
+cursor = conn.cursor()
+username=""
+types=""
+sunnylisten=""
+sunnymood=""
+rainylisten=""
+rainymood=""
+cloudylisten=""
+cloudymood=""
+workout=""
+working=""
+relaxing=""
+#C:\Users\zidel\Documents\GitHub\dJams\templates
+print("C:\\Users\\zidel\\Documents\\GitHub\\dJams\\templates\\example.json")
+with open('C:\\Users\\zidel\\Documents\\GitHub\\dJams\\templates\\example.json') as survey:
+    surveyAnswers = json.loads(survey.read())
+    print(surveyAnswers)
+
+for info in surveyAnswers:
+    userID=surveyAnswers['Username']
+    types=surveyAnswers['types']
+    sunnylisten=surveyAnswers['sunnylisten']
+    sunnymood=surveyAnswers['sunnymood']
+    rainylisten=surveyAnswers['rainylisten']
+    rainymood=surveyAnswers['rainymood']
+    cloudylisten=surveyAnswers['cloudylisten']
+    cloudymood=surveyAnswers['cloudymood']
+    workout=surveyAnswers['workout']
+    working=surveyAnswers['working']
+    relaxing=surveyAnswers['relaxing']
+    email = surveyAnswers['email']
+print(email)
+
+cursor.execute("INSERT INTO Survey (UserID, types, sunnylisten, sunnymood, rainylisten, rainymood, cloudylisten, cloudymood, workout, relaxing,working) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+(userID, types, sunnylisten, sunnymood, rainylisten, rainymood, cloudylisten, cloudymood, workout, relaxing, working))
+conn.commit()
+
+cursor.close()
+conn.close()
+# sql_command = "INSERT INTO Survey (UserID, types, sunnylisten, sunnymood, rainylisten, rainymood, cloudylisten, cloudymood, workout, working, relaxing) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?,?);"
+#
+# cursor.execute(sql_command,(username, types, sunnylisten, sunnymood, rainylisten, rainymood, cloudylisten, cloudymood, workout, working, relaxing))
