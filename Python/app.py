fe28bf85285cdcf85b2acc0d11f3c597e0a14fef
Daniel Zidelis <zidelisd@wit.edu>
2018-08-07 10:14:19 -0400

    updates to test

--- Python/app.py
+++ Python/app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 
 @app.route("/myflaskapp.py")
 
+@app.route("/Google")
+def google():
+    helly = os.system('python hello.py')
+    return helly
+
 def get_x():
     x = 2
     return x
