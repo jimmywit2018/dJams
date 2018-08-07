13840f38fc86cca5e05266ed9e508f1750a35fd1
Daniel Zidelis <zidelisd@wit.edu>
2018-08-06 20:34:18 -0400

    GoogleCalendar and surveyanswers scripts working
    They now are able to push to the database after being run.

--- Python/GoogleCalendar.py
+++ Python/GoogleCalendar.py
@@ -4,6 +4,23 @@ from apiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file as oauth_file, client, tools
 import os
+import pymongo
+from pymongo import MongoClient
+from pprint import pprint
+import json
+import re
+
+client = pymongo.MongoClient("mongodb+srv://dannyzidelis:DJAMSdurga1!@djamscluster-u6bx0.mongodb.net/dJamsDB")
+
+db = client.dJamsDB
+
+coll = db.ScheduledTimes
+with open(r'C:\Users\zidel\Documents\GitHub\dJams\templates\example.json') as infile:
+     example = json.loads(infile.read())
+email = example['email'][0]
+# print(email)
+#print(client.database_names())
+#print(coll)
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
@@ -17,8 +34,8 @@ def main():
 
     currentdate= currentdatetime[:-16]
     currenttime= currentdatetime[11:-7]
-    print(currentdatetime)
-    print(currentdate, currenttime)
+    # print(currentdatetime)
+    # print(currentdate, currenttime)
     #print(dir_path)
     #files = [f for f in os.listdir(dir_path) if os.path.isfile(f)]
     #for f in files:
@@ -36,7 +53,7 @@ def main():
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 20 events')
+    # print('Getting the upcoming 20 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
                                           maxResults=20, singleEvents=True,
                                           orderBy='startTime').execute()
@@ -53,10 +70,32 @@ def main():
         end = event['end'].get('dateTime', event['end'].get('date'))
         enddate= end[:-15]
         endtime= end[11:-6]
-        if(startdate==enddate):
-            print(startdate, starttime, "-",endtime, event['summary'])
-        else:
-            print(startdate,starttime,"-",enddate,endtime, event['summary'])
+        #prints out all the data
+        # if(startdate==enddate):
+        #     print(startdate, starttime, "-",endtime, event['summary'])
+        # else:
+        #     print(startdate,starttime,"-",enddate,endtime, event['summary'])
+
+        #selects what goes into the database
+        if event['summary'].upper() == "WORK":
+            sched_rec = {"email": email, "name":event['summary'], "startdate": startdate, "enddate": enddate, "starttime":starttime,"endtime":endtime}
+            rec_id = coll.insert_one(sched_rec)
+            #print("Data inserted with record ids", rec_id)
+        elif event['summary'].upper() == "WORK OUT":
+            sched_rec = {"email": email, "name":event['summary'], "startdate": startdate, "enddate": enddate, "starttime":starttime,"endtime":endtime}
+            rec_id = coll.insert_one(sched_rec)
+            #print("Data inserted with record ids", rec_id)
+        elif event['summary'].upper() == "GYM":
+            sched_rec = {"email": email, "name":event['summary'], "startdate": startdate, "enddate": enddate, "starttime":starttime,"endtime":endtime}
+            rec_id = coll.insert_one(sched_rec)
+            #print("Data inserted with record ids", rec_id)
+        elif event['summary'].upper() == "RELAX":
+            sched_rec = {"email": email, "name":event['summary'], "startdate": startdate, "enddate": enddate, "starttime":starttime,"endtime":endtime}
+            rec_id = coll.insert_one(sched_rec)
+            #print("Data inserted with record ids", rec_id)
+    # cursor = coll.find()
+    # for record in cursor:
+    #     print(record)
     # print(startdate, starttime)
     # print(enddate, endtime)
 
