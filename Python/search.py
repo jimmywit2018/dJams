c958a0a3b9b056557eb33f12f8daca6ccccd251b
Daniel Zidelis <zidelisd@wit.edu>
2018-08-06 23:35:46 -0400

    Search.py now gets the playlists that we create and populates the db

--- Python/search.py
+++ Python/search.py
@@ -7,13 +7,33 @@ import requests
 import json
 import mmap
 import re
+import pymongo
+from pymongo import MongoClient
+import pprint
+from bson import ObjectId
+from bson import BSON
+from bson import json_util
 
-scope = "playlist-modify-public"
-username = 'h0m596l5gz014wayiyy29p0gg'
+client = pymongo.MongoClient("mongodb+srv://dannyzidelis:DJAMSdurga1!@djamscluster-u6bx0.mongodb.net/dJamsDB")
+db = client.dJamsDB
+coll_in = db.MusicPref
+coll_out =db.Spotify
 
+with open('currentuser.txt', 'r') as myfile:
+    email=myfile.read().replace('\n', '')
+# print(email)
+query_results = coll_in.find_one({"email": email})
+query_results = json.dumps(query_results, indent=4, default=json_util.default)
+query_results = json.loads(query_results)
+# print(query_results)
+#print(query_results['username'])
 
+username = query_results['username']
 client_id = 'b7642ea152d44cbf95e9d7efd223cc49'
 client_secret = '1094e61f08a845a6b1e9a651fe9a1e2b'
+
+scope = 'playlist-modify-public'
+
 token = util.prompt_for_user_token(username, scope, client_id=client_id, client_secret = client_secret , redirect_uri="http://google.com/")
 
 sp = spotipy.Spotify(auth=token)
@@ -36,7 +56,19 @@ for track in dataplaylist['items']:
         playlist_name = dataplaylist['items'][iter]['name']
         playlist_uri= dataplaylist['items'][iter]['uri']
         playlist_id = dataplaylist['items'][iter]['id']
-        print(playlist_name, playlist_uri, playlist_id)
+        # print(playlist_name, playlist_uri, playlist_id)
+        if playlist_name == "Relax":
+            playlist_rec = {"username": username, "email": email, "Playlist Name":"Relax", "PlaylistID": playlist_id, "PlaylistURI": playlist_uri}
+            rec_id = coll_out.insert_one(playlist_rec)
+        elif playlist_name == "Rain":
+            playlist_rec = {"username": username, "email": email, "Playlist Name":"Rain", "PlaylistID": playlist_id, "PlaylistURI": playlist_uri}
+            rec_id = coll_out.insert_one(playlist_rec)
+        elif playlist_name == "Work Out":
+            playlist_rec = {"username": username, "email": email, "Playlist Name":"Work Out", "PlaylistID": playlist_id, "PlaylistURI": playlist_uri}
+            rec_id = coll_out.insert_one(playlist_rec)
+        elif playlist_name == "Work":
+            playlist_rec = {"username": username, "email": email, "Playlist Name":"Work", "PlaylistID": playlist_id, "PlaylistURI": playlist_uri}
+            rec_id = coll_out.insert_one(playlist_rec)
         iter= iter+1
 # print(items)
 # outfile.close()
